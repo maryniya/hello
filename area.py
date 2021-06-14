52012a847c5b92ca6ca52df769ceec0daa0c004c
maryniya <85816017+maryniya@users.noreply.github.com>
2021-06-14 15:16:24 +0530

    files: area and extension
    the above files were uploaded

---
+++ area.py
@@ -0,0 +1,7 @@
+import math
+
+radius = float(input("Input the radius of the circle:"))
+area = math.pi*radius*radius
+
+print("The area of the circle with radius 1.1 is:")
+print(area)
---
+++ extension.py
@@ -0,0 +1,7 @@
+fileName = input("Input the filename:")
+
+file_extns = fileName.split(".")
+
+print("The extension of the file is:" + repr(file_extns[-1]))
+
+
